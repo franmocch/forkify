8d611241119a2f369e6d1fdcf67e4e778b3d83ce
franmocch <fimocchegiani@gmail.com>
2022-06-27 18:44:34 +0200

    Library added version

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -1,6 +1,6 @@
 import View from './View.js';
 import icons from '../../img/icons.svg';
-import { Fraction } from 'fractional';
+import { Fraction } from '../libraryHelper/fractional/index.js';
 
 class recipeView extends View {
   _parentElement = document.querySelector('.recipe');
